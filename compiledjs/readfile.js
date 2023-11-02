95b3f549ac7744e792870dd1d3f533d548dcb917
jeremy <jcope0787@protonmail.com>
2023-11-01 18:08:35 -0700

    added create element lines ofr file input

--- compiledjs/readfile.js
+++ compiledjs/readfile.js
@@ -1,1 +1,8 @@
 // * This file serves to read the users flashcard file
+document.body.onload = addElements;
+function addElements() {
+    // Code for file input
+    var uploadDiv = document.createElement("div");
+    var uploadText = document.createElement("p");
+    var uploadButton = document.createElement("input");
+}
--- src/readfile.ts
+++ src/readfile.ts
@@ -1,1 +1,9 @@
 // * This file serves to read the users flashcard file
+document.body.onload = addElements;
+function addElements():void 
+{
+    // Code for file input
+    const uploadDiv:HTMLDivElement = document.createElement("div");
+    const uploadText:HTMLParagraphElement = document.createElement("p");
+    const uploadButton:HTMLInputElement = document.createElement("input");
+}
