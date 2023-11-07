1fb5570401c0a1502f95a6b348fff7747be6ccb9
jman45-9 <jeremy.cope7142@gmail.com>
2023-11-06 20:04:02 -0700

    added start quiz btn

--- src/quiz.ts
+++ src/quiz.ts
@@ -1,12 +1,15 @@
 import { cardSet } from "./readfile.js";
 
+const startBtn:HTMLButtonElement = document.createElement("button");
+
 function checkForSet():void
 {
     if (cardSet.complete != true)
         return;
     
     clearInterval(cardSetInterval);
-    
+
+    startBtn.textContent = "Start Quiz";
 }
 
 var cardSetInterval:number = setInterval(checkForSet, 10);
